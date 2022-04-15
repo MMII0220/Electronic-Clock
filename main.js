bccbf98d80192345463d0fee4e38e52342feaa0f
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2022-04-15 18:37:05 +0300

    finished html css

---
+++ main.js
@@ -0,0 +1,10 @@
+let day = document.querySelector('.day'),
+  hour = document.querySelector('.hour'),
+  minute = document.querySelector('.minute'),
+  meridiem = document.querySelector('.meridiem');
+
+function timer() {
+    day = 
+}
+
+setInterval(timer, 1000);
