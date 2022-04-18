6bfd947893c0d123580b4cece346844d96999e7d
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2022-04-18 13:59:58 +0300

    finished project

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 let day = document.querySelector('.day'),
-  hour = document.querySelector('.hour'),
-  minute = document.querySelector('.minute'),
-  meridiem = document.querySelector('.meridiem');
+  hours = document.querySelector('.hour'),
+  minutes = document.querySelector('.minute'),
+  meridiem = document.querySelector('.meridiem'),
+  today = new Date();
 
-function timer() {
-    day = 
+let week = {
+  1: 'MON',
+  2: 'TUE',
+  3: 'WED',
+  4: 'THU',
+  5: 'FRI',
+  6: 'SAT',
+  7: 'SUN',
+};
+
+function time(t) {
+  d = t.getDay();
+  h = t.getHours();
+  m = t.getMinutes();
+  mer = t.getHours();
+
+  day.innerHTML = `${week[d]}`;
+
+  if (h > 9) hours.innerHTML = `${h}`;
+  else hours.innerHTML = `0${h}`;
+
+  if (m > 9) minutes.innerHTML = `${m}`;
+  else minutes.innerHTML = `0${m}`;
+
+  if (mer > 11) meridiem.innerHTML = `PM`;
+  else meridiem.innerHTML = `AM`;
 }
 
-setInterval(timer, 1000);
+time(today);
